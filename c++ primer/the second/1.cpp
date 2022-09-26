1fb612935d49792210e7410bbfe6b1ab3145a56e
Yazhi201 <yazhi201@gmail.com>
2022-09-26 10:59:00 -0400

    9.26/10:58

--- c++ primer/the second/1.cpp
+++ c++ primer/the second/1.cpp
@@ -10,10 +10,20 @@ int main() {
     // nod j;
     // int i = 0;
     // cout << i << ' ' << ::i << endl;
-    int ival = 1024;
-    int &refval = ival;
-    refval = 1000;
-    cout << refval << ' ' << ival << endl;    
-    // int ival;   
+    // int ival = 1024;
+    // int &refval = ival; 
+    // refval = 1000;
+    // cout << refval << ' ' << ival << endl;    
+    // int *ival = 0;   
+    // cin >> ival; 
+    // cout << ival << endl;
+    // int *it = &ival;
+    // cout << it << endl;
+    // cout << "Hello World\n";
+    // cout << *ival << endl;
+    int i = 42;
+    int *p1 = &i;
+    *p1 = *p1 * *p1;
+    cout << *p1 << endl;
 }
 
